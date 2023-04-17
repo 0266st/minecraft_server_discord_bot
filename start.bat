e511bcc3144866c03f5c69482d4972cd476e24bb
0266st <yoyuhihami@gmal.com>
2023-04-17 12:56:32 +0900

    [緊急] 修正パッチv1.2.1

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo on
 cd server_scripts
 title minecraft_server
-java -Xmx12G -Xms12G -jar server.jar -nogui
+java -Xmx12G -Xms12G -jar C:\Minecraft_servers\server_scripts\server.jar -nogui
 call ./backup.bat
 exit
