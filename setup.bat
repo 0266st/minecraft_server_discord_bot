f92349a44de65117aec53ca4a70744b66f39cf40
0266st <108959915+0266st@users.noreply.github.com>
2023-09-18 12:21:50 +0900

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -24,6 +24,6 @@ set "converted_path=%JARFILE_PATH:\=/%"
 echo ロード中です。しばらくお待ちください....
 echo 注意! settings.jsonにはDiscordのBotトークンが含まれています。"絶対に"公開しないでください。
 
-echo {"user_id":"%USERID%","server_jar_path":"%converted_path%", "channel_id":"%CHANNEL_ID%", "server_address":"%SERVER_ADDRESS%", "rcon_port":"%RCON_PORT%", "rcon_pass":"%RCON_PASS%"} > settings.json
+echo {"user_id":"%USERID%","server_jar_path":"%converted_path%", "channel_id":"%CHANNEL_ID%", "server_address":"%SERVER_ADDRESS%", "rcon_port":"%RCON_PORT%", "rcon_pass":"%RCON_PASS%", "TOKEN":"%DISCORD_BOT_TOKEN%"} > settings.json
 echo セットアップが完了しました。何かキーを押して退出してください.
 pause
