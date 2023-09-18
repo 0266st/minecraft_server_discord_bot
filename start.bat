6730e3a4d8b0b52825f68168e215e84a41f9e9da
0266st <108959915+0266st@users.noreply.github.com>
2023-09-18 14:17:09 +0900

    start.bat の更新

--- start.bat
+++ start.bat
@@ -1,8 +1,3 @@
-@echo on
-cd C:\minecraft_servers\playit
-start C:\minecraft_servers\playit\playit.exe
-cd C:\minecraft_servers\server_scripts
+@echo off
 title minecraft_server
 java -Xmx12G -Xms12G -jar %%1 -nogui
-call ./backup.bat
-exit
